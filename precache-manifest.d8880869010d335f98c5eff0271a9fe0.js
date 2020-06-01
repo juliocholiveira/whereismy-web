c783b668e0bbf18044da3c8baa51ee99c406963c
Júlio César <juliocholiveira@gmail.com>
2020-06-01 15:55:37 -0300

    Updates

---
+++ precache-manifest.d8880869010d335f98c5eff0271a9fe0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b3b63cbbeef651e38ad9fd65dc8526e",
+    "url": "/where-are-you-web/index.html"
+  },
+  {
+    "revision": "2e05746ecfb0950355d6",
+    "url": "/where-are-you-web/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "be20a150d40f95d023ee",
+    "url": "/where-are-you-web/static/js/2.9464ffbc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/where-are-you-web/static/js/2.9464ffbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e05746ecfb0950355d6",
+    "url": "/where-are-you-web/static/js/main.d55ba3ed.chunk.js"
+  },
+  {
+    "revision": "5990ab98a9b8b9b9738d",
+    "url": "/where-are-you-web/static/js/runtime-main.fae685f6.js"
+  }
+]);
